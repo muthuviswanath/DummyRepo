6e63c2300c57a569a9b0eebf890d87511e162d16
muthuviswanath <muthuviswanath@outlook.com>
2024-05-17 19:30:10 +0530

    Assignment

--- TelephoneCombination.java
+++ TelephoneCombination.java
@@ -1,7 +1,3 @@
-public class TelephoneCombination {
-    
-}
-
 /*
  * 2 ABC
  * 3 DEF
@@ -16,3 +12,32 @@ public class TelephoneCombination {
  * Output [AD, AE, AF, BD, BE, BF, CD, CE, CF]
  * 
  */
+import java.util.*;
+public class TelephoneCombination {
+    
+    public static List<String> combinations(String digits){
+        List<String> result = new ArrayList<String>();
+        if(digits.length() == 0){
+            return result;
+        }
+        result.add("");
+        String[] d = new String[]{"abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"};
+        for(char i:digits.toCharArray()){
+            String s = d[i-'2'];
+            List<String> t = new ArrayList<>();
+            for (String a: result){
+                for(String b: s.split("")){
+                    t.add(a+b);
+                }
+            }
+            result = t;
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        List result = combinations("23");
+        System.out.println(result);
+    }
+}
+
